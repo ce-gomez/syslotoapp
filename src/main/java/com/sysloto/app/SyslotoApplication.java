4d4c1758c53a8c86e5213b5b8b6a9c403ffa540b
deggcoder <cesar.gomezguido@gmail.com>
2025-12-23 22:09:48 -0600

    Init project

---
+++ src/main/java/com/sysloto/app/SyslotoApplication.java
@@ -0,0 +1,13 @@
+package com.sysloto.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyslotoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SyslotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sysloto/app/SyslotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysloto.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyslotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
