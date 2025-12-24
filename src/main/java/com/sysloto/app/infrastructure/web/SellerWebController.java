0a6362e433d1e19d3c86518d9af20f63c7b6bb07
deggcoder <cesar.gomezguido@gmail.com>
2025-12-24 11:25:55 -0600

    Update seller profile

--- src/main/java/com/sysloto/app/infrastructure/web/SellerWebController.java
+++ src/main/java/com/sysloto/app/infrastructure/web/SellerWebController.java
@@ -1,11 +1,13 @@
 package com.sysloto.app.infrastructure.web;
 
 import com.sysloto.app.application.service.RegisterSellerService;
+import com.sysloto.app.application.service.UpdateSellerProfile;
 import com.sysloto.app.domain.seller.SellerRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class SellerWebController {
 
     private final RegisterSellerService registerSellerService;
+    private final UpdateSellerProfile updateSellerProfile;
     private final SellerRepository sellerRepository;
 
     @GetMapping
@@ -31,9 +34,26 @@ public class SellerWebController {
 
     @PostMapping("/register")
     public String registerSeller(@RequestParam String name,
-                                 @RequestParam String lastname,
-                                 @RequestParam double factor) {
+            @RequestParam String lastname,
+            @RequestParam double factor) {
         registerSellerService.register(name, lastname, factor);
+        return "redirect:/sellers";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable Long id, Model model) {
+        var seller = sellerRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid seller Id:" + id));
+        model.addAttribute("seller", seller);
+        return "sellers/edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String updateSeller(@PathVariable Long id,
+            @RequestParam String name,
+            @RequestParam String lastname,
+            @RequestParam double factor) {
+        updateSellerProfile.updateProfile(id, name, lastname, factor);
         return "redirect:/sellers";
     }
 }
