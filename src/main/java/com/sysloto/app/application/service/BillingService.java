2f7b8fda61b6b4e177d2f35a84e1a99b4563766b
deggcoder <cesar.gomezguido@gmail.com>
2025-12-24 19:20:20 -0600

    Adding billing service

---
+++ src/main/java/com/sysloto/app/application/service/BillingService.java
@@ -0,0 +1,24 @@
+package com.sysloto.app.application.service;
+
+import com.sysloto.app.domain.sale.Bill;
+import com.sysloto.app.domain.sale.BillRepository;
+import com.sysloto.app.domain.seller.SellerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+@AllArgsConstructor
+public class BillingService {
+    private final BillRepository billRepository;
+    private final ScheduleFinder scheduleFinder;
+    private final SellerRepository sellerRepository;
+
+    @Transactional
+    public Bill registerNewBill(Long sellerId) {
+        var seller = sellerRepository.findById(sellerId).get();
+        var schedule = scheduleFinder.findCurrentSchedule()
+                .orElseThrow(() -> new IllegalStateException("Fuera de turno"));
+        var bill = Bill.create(seller, schedule.getScheduleId());
+        return billRepository.save(bill);
+    }
+
+}
